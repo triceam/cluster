b22a7cc20f5a717161fc056e89d97728fc712c96
Andrew Trice <triceam@gmail.com>
2021-03-01 16:05:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "ibm_container_cluster" "cluster" {
   kube_version      = "${var.kube_version}"
   public_vlan_id    = "${var.public_vlan_id}"
   private_vlan_id   = "${var.private_vlan_id}"
+  resource_group_name = "${var.resource_group_name}"
 }
 
 resource "random_id" "name" {
