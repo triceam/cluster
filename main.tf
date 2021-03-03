910cfb11255d9d94f66d74c307106c232ed7efa0
Andrew Trice <triceam@gmail.com>
2021-03-03 15:35:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,24 @@
-resource "ibm_container_cluster" "cluster" {
-  name              = "${var.cluster_name}${random_id.name.hex}"
-  datacenter        = "${var.datacenter}"
-  default_pool_size = "${var.default_pool_size}"
-  machine_type      = "${var.machine_type}"
-  hardware          = "${var.hardware}"
-  kube_version      = "${var.kube_version}"
-  public_vlan_id    = "${var.public_vlan_id}"
-  private_vlan_id   = "${var.private_vlan_id}"
-  resource_group_id   = "${data.ibm_resource_group.resourceGroup.id}"
+
+# Template data Variables
+data "ibm_resource_group" "resource_group" {
+  name = var.resource_group_name
+}
+  
+resource "ibm_container_cluster" "create_cluster" {
+  name              = var.name
+  datacenter        = var.datacenter
+  default_pool_size = var.default_pool_size
+  machine_type      = var.machine_type
+  hardware          = var.hardware
+  resource_group_id = data.ibm_resource_group.resource_group.id
+  kube_version      = var.kube_version
+  public_vlan_id    = var.public_vlan_id
+  private_vlan_id   = var.private_vlan_id
+  entitlement       = var.entitlement
 }
 
-data "ibm_resource_group" "resourceGroup" {
-  name     = "${var.resource_group_name}"
-}
 
-resource "random_id" "name" {
-  byte_length = 4
+data "ibm_container_cluster_config" "config" {
+  cluster_name_id = ibm_container_cluster.create_cluster.id
+  resource_group_id = data.ibm_resource_group.resource_group.id
 }
