a8d77be7b7087a907be66d084fb389f1b4e17068
Andrew Trice <triceam@gmail.com>
2021-03-03 15:54:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,3 +22,12 @@ data "ibm_container_cluster_config" "config" {
   cluster_name_id = ibm_container_cluster.create_cluster.id
   resource_group_id = data.ibm_resource_group.resource_group.id
 }
+
+//Variable required for content catalog to select terraform version
+variable "TF_VERSION" {
+  default = "0.12"
+  description = "terraform engine version to be used in schematics"
+}
+terraform {
+  required_version = "> 0.12.0"
+}
