35e1f6acd84174ae1dd1fbfd9ed2ce63a9200241
Andrew Trice <triceam@gmail.com>
2021-03-08 14:12:31 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,8 @@
-provider "ibm" {
-  ibmcloud_api_key   = var.ibmcloud_api_key
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+    }
+  }
 }
